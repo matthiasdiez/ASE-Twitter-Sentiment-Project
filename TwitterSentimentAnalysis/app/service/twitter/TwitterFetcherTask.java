939208d6d22698b0792d343971a6413094d543b3
Christian Bosshard <cbossi1987@gmail.com>
2014-04-14 11:40:23 +0200

    Remove warnings and play-default-tests.

--- TwitterSentimentAnalysis/app/service/twitter/TwitterFetcherTask.java
+++ TwitterSentimentAnalysis/app/service/twitter/TwitterFetcherTask.java
@@ -5,28 +5,25 @@ import java.util.List;
 import java.util.Set;
 import java.util.TimerTask;
 
-import play.libs.WS;
 import model.core.Analysis;
 import model.core.Term;
 import model.repositories.AnalysisRepository;
-import twitter4j.Twitter;
-import twitter4j.TwitterFactory;
-import twitter4j.conf.ConfigurationBuilder;
+import play.libs.WS;
 
 public class TwitterFetcherTask extends TimerTask {
 
-	@Override
-	public void run() {
-		final Set<Term> activeTerms = new HashSet<Term>();
-		final List<Analysis> analyses = AnalysisRepository.INSTANCE.all();
-		for (final Analysis analysis : analyses) {
-			if (analysis.isActive()) {
-				activeTerms.addAll(analysis.getTerms());
-			}
-		}
-		for (final Term term : activeTerms) {
-			WS.url("http://awseb-e-d-awsebloa-sa5puxjlfpjr-1662706129.eu-west-1.elb.amazonaws.com/run/" + term.getId()).get();
-		}
-		Thread.yield();
-	}
+  @Override
+  public void run() {
+    final Set<Term> activeTerms = new HashSet<Term>();
+    final List<Analysis> analyses = AnalysisRepository.INSTANCE.all();
+    for (final Analysis analysis : analyses) {
+      if (analysis.isActive()) {
+        activeTerms.addAll(analysis.getTerms());
+      }
+    }
+    for (final Term term : activeTerms) {
+      WS.url("http://awseb-e-d-awsebloa-sa5puxjlfpjr-1662706129.eu-west-1.elb.amazonaws.com/run/" + term.getId()).get();
+    }
+    Thread.yield();
+  }
 }
