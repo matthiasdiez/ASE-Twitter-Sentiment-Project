b35059b431b96506fbf52d7816845d6ec565bbb6
Christian Bosshard <cbossi1987@gmail.com>
2014-04-14 11:39:25 +0200

    Remove warning

--- TwitterSentimentAnalysisREST/app/service/sentiment/SentimentClassifier.java
+++ TwitterSentimentAnalysisREST/app/service/sentiment/SentimentClassifier.java
@@ -14,51 +14,55 @@ import com.aliasi.classify.ConditionalClassification;
 import com.aliasi.classify.LMClassifier;
 import com.aliasi.util.AbstractExternalizable;
 
+@SuppressWarnings("rawtypes")
 public class SentimentClassifier {
 
-	String[] categories;
-	LMClassifier classifier;
+  String[] categories;
+  LMClassifier classifier;
 
-	public SentimentClassifier() {
+  public SentimentClassifier() {
 
-		try {
-			InputStream inputStream = Play.application().resourceAsStream("resources/classifier.txt");
-			final File tempFile = File.createTempFile("classifier", "txt");
-			tempFile.deleteOnExit();
-			try (FileOutputStream out = new FileOutputStream(tempFile)) {
-				IOUtils.copy(inputStream, out);
-			}
-			classifier = (LMClassifier) AbstractExternalizable.readObject(tempFile);
-			categories = classifier.categories();
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+    try {
+      final InputStream inputStream = Play.application().resourceAsStream("resources/classifier.txt");
+      final File tempFile = File.createTempFile("classifier", "txt");
+      tempFile.deleteOnExit();
+      try (FileOutputStream out = new FileOutputStream(tempFile)) {
+        IOUtils.copy(inputStream, out);
+      }
+      classifier = (LMClassifier) AbstractExternalizable.readObject(tempFile);
+      categories = classifier.categories();
+    }
+    catch (final ClassNotFoundException e) {
+      e.printStackTrace();
+    }
+    catch (final IOException e) {
+      e.printStackTrace();
+    }
 
-	}
+  }
 
-	private String classify(String text) {
-		ConditionalClassification classification = classifier.classify(text);
-		return classification.bestCategory();
-	}
+  private String classify(final String text) {
+    final ConditionalClassification classification = classifier.classify(text);
+    return classification.bestCategory();
+  }
 
-	public double getSentimentScore(ArrayList<String> tweetList) {
-		double score = 0;
-		for (String tweet : tweetList) {
-			String sentiment = classify(tweet);
-			if (sentiment.equals("pos")) {
-				score += 1;
-			} else if (sentiment.equals("neg")) {
-				score += 0;
-			} 
-//			else { // sentiment.equals("neu")){
-//				score += 0.5;
-//			}
-		}
+  public double getSentimentScore(final ArrayList<String> tweetList) {
+    double score = 0;
+    for (final String tweet : tweetList) {
+      final String sentiment = classify(tweet);
+      if (sentiment.equals("pos")) {
+        score += 1;
+      }
+      else if (sentiment.equals("neg")) {
+        score += 0;
+      }
+      // else { // sentiment.equals("neu")){
+      // score += 0.5;
+      // }
+    }
 
-		score = score / tweetList.size();
-		return score;
-	}
+    score = score / tweetList.size();
+    return score;
+  }
 
 }
